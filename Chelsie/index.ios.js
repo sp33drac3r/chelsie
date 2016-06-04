9b935993f3c26ec4bbaa7c587a404a144b7e2050
portatlas <a.vathanaka@gmail.com>
2016-06-04 15:29:36 -0700

    Switch from NavigatorIOS to Navigator.  Add renderScene function to control routes.

--- Chelsie/index.ios.js
+++ Chelsie/index.ios.js
@@ -9,45 +9,56 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  Navigator,
   View
 } from 'react-native';
 
+import Resource from './App/Components/Resource'
+
 class Chelsie extends Component {
+  renderScene(route, navigator){
+    if (route.name == 'Resource') {
+      return <Resource navigator={navigator} />
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Navigator
+        initialRoute={{ name: 'Resource' }}
+        renderScene={ this.renderScene.bind(this) }
+      />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
+var styles = StyleSheet.create({
+  other: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#aaeaca'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  word: {
+    fontFamily: 'Cochin',
+    color: '#000000',
+    fontSize: 30,
+    fontWeight: 'bold'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  // container: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   backgroundColor: '#F5FCFF',
+  // },
+  // welcome: {
+  //   fontSize: 20,
+  //   textAlign: 'center',
+  //   margin: 10,
+  // },
+  // instructions: {
+  //   textAlign: 'center',
+  //   color: '#333333',
+  //   marginBottom: 5,
+  // },
 });
 
 AppRegistry.registerComponent('Chelsie', () => Chelsie);
