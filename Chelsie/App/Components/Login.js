ee24ba1afc360c89cc8e66cfcba9bcea5ea0a9f1
ftBessmann <a.ll@comtechnics.by>
2016-06-09 15:09:05 -0700

    add styling to login page

--- Chelsie/App/Components/Login.js
+++ Chelsie/App/Components/Login.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text,
   StyleSheet,
+  Text,
+  TextInput,
   ListView,
   TouchableHighlight,
-  TextInput,
   Navigator,
   Alert,
   AsyncStorage,
@@ -64,78 +64,50 @@ class Login extends Component {
 
   render(){
     return(
-      <View style={styles.container}>
-      <Image source={require('./../../imgs/gradient3.jpg')} style={styles.backgroundImage} />
-      <Text style={styles.header}>Chelsie</Text>
-      <Text style={styles.redirectMessage}>{this.state.message}</Text>
-      <TextInput
-        style={styles.loginArea}
-        placeholder="EMAIL"
-        onChangeText={(email) => this.setState({email: email})}
-        value={this.state.email}
-      />
-      <TextInput
-        secureTextEntry={true}
-        placeholder="PASSWORD"
-        style={styles.loginArea}
-        onChangeText={(password) => this.setState({password: password})}
-        value={this.state.password}
-      />
-      <TouchableHighlight onPress={this.login.bind(this)} style={styles.button}>
-        <Text style={styles.buttonText}>
-          Log In
-        </Text>
-      </TouchableHighlight>
-      <TouchableHighlight onPress={() => {this.props.navigator.resetTo({name: 'SignUp'})}}>
-        <Text style={styles.newUserText}>New to Chelsie? Create an account.</Text>
-      </TouchableHighlight>
-      </View>
+      <Image source={require('./../../imgs/gradient3.jpg')} style={styles.backgroundImage}>
+        <View style={styles.container}>
+          <Text style={styles.text}>email</Text>
+          <TextInput
+            style={styles.loginArea}
+            onChangeText={(email) => this.setState({email: email})}
+            value={this.state.email}
+          />
+          <Text style={styles.text}>password</Text>
+          <TextInput
+            secureTextEntry={true}
+            style={styles.loginArea}
+            onChangeText={(password) => this.setState({password: password})}
+            value={this.state.password}
+          />
+          <TouchableHighlight onPress={this.login.bind(this)} style={styles.button}>
+            <Text style={styles.text}>Log In</Text>
+          </TouchableHighlight>
+          <TouchableHighlight onPress={() => {this.props.navigator.resetTo({name: 'SignUp'})}}>
+            <Text style={styles.text}>Create an account</Text>
+          </TouchableHighlight>
+        </View>
+      </Image>
     )
   }
 }
 
 var styles = StyleSheet.create({
-  backgroundImage: {
-    flex: 1,
-    // resizeMode: 'cover',
-    alignItems: 'center',
-    width: null,
-    height: null,
-    justifyContent: 'center'
-
-  },
   container: {
-    flex: 1,
-    // flexDirection: 'column',
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // backgroundColor: '#F5FCFF',
+    top: 200,
+    flex: 0.05,
+    width: 415,
+    alignItems: 'center',
   },
   buttonText: {
     fontSize: 18,
-    color: '#111',
+    color: '#FFFFFF',
     alignSelf: 'center'
   },
   button: {
     height: 45,
     flexDirection: 'column',
-    // backgroundColor: '#F0F3BD',
-    borderColor: 'grey',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 10,
-    marginTop: 10,
-    marginLeft: 5,
-    marginRight: 5,
-    alignSelf: 'center',
-    justifyContent: 'center'
-  },
-  loginArea: {
-    height: 45,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    // backgroundColor: '#ECF0F1',
-    borderColor: 'grey',
+    backgroundColor: 'transparent',
+    borderColor: '#FFFFFF',
     borderWidth: 1,
     borderRadius: 8,
     marginBottom: 10,
@@ -143,25 +115,30 @@ var styles = StyleSheet.create({
     marginLeft: 5,
     marginRight: 5,
     alignSelf: 'stretch',
+    justifyContent: 'center',
+  },
+  loginArea: {
+    height: 45,
+    color: '#FFFFFF',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    borderColor: '#FFFFFF',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 10,
+    marginLeft: 5,
+    marginRight: 5,
+    paddingLeft: 20,
+    alignSelf: 'stretch',
     justifyContent: 'center'
   },
-  header: {
-    fontWeight: 'bold',
-    fontSize: 40,
+  text: {
     fontFamily: 'Apple SD Gothic Neo',
-    alignSelf: 'center'
-  },
-  redirectMessage: {
-    fontSize: 20,
-    fontFamily: 'Apple SD Gothic Neo',
+    color: '#FFFFFF',
+    fontSize: 17,
+    fontWeight: 'normal',
     alignSelf: 'center',
-    color: 'red'
-  },
-  newUserText: {
-    fontSize: 18,
-    color: 'blue',
-    alignSelf: 'center',
-    textDecorationLine: 'underline'
   },
 });
 
