3929631240e9494741bb18f7c483077eae0562c2
portatlas <a.vathanaka@gmail.com>
2016-07-14 11:52:30 +0700

    Find screen width and change about us to be within screen width

--- Chelsie/App/Components/AboutUs.js
+++ Chelsie/App/Components/AboutUs.js
@@ -4,16 +4,21 @@ import {
   View,
   Text,
   StyleSheet,
-  Image
+  Image,
+  ScrollView
 } from 'react-native';
+
+const SCREEN_WIDTH = require('Dimensions').get('window').width;
 
 class AboutUs extends Component {
   render(){
     return (
       <Image source={require('./../../imgs/gradient3.jpg')} style={styles.backgroundImage}>
       <View style={styles.container}>
+        <ScrollView>
         <Text style={styles.header}>ABOUT CHELSIE</Text>
         <Text style={styles.text}>Chelsie is a community for survivors of college sexual assault. Too often, survivors are left with few resources and feelings of loneliness or detachment, not knowing who they can reach out to for help or advice. Through anonymous posts, survivors can share their stories and journal their healing process, giving their support back to the community.  This community borrows its name from a former student of Kenyon College who was sexually assaulted in November of 2015. She went public with her story through an open letter to Kenyon written by her brother, also an alum.  An anonymous community is important for the protection of survivors but anonymity online brings risk of abuse. We rely on the community to flag hateful or abusive language so it can be removed. This is a safe space. < /Text>
+        </ScrollView>
       </View>
       </Image>
     )
@@ -23,7 +28,7 @@ class AboutUs extends Component {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: 400,
+    width: SCREEN_WIDTH - 10,
     paddingTop: 90,
     paddingLeft: 14,
     flexDirection: 'column',
